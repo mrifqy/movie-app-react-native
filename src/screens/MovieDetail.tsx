12da1e9e572c6555c3a751d99577977d9d71b824
mrifqy <muhammad.rifqy2002@gmail.com>
2024-06-28 11:08:25 +0700

    setup env variables

--- src/screens/MovieDetail.tsx
+++ src/screens/MovieDetail.tsx
@@ -1,18 +1,41 @@
+import React from 'react'
+import { View, Text, Button } from 'react-native'
+import { API_URL, API_ACCESS_TOKEN } from '@env' // Ditambahkan
 
+const MovieDetail = ({ navigation }: any): any => {
+  const fetchData = (): void => {
+    if (API_URL == null || API_ACCESS_TOKEN.length == null) {
+      throw new Error('ENV not found')
+    }
 
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+    const options = {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        Authorization: `Bearer ${API_ACCESS_TOKEN}`,
+      },
+    }
 
-const MovieDetail = ({ navigation }: any) => {
+    fetch(API_URL, options)
+      .then((response) => response.json())
+      .then((response) => {
+        console.log(response)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+  }
   return (
-    <View>
-      <Text>Movie Detail Screen</Text>
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Movie Detail Page</Text>
       <Button
-        title="Go back to Home"
-        onPress={() => navigation.navigate('Home')}
+        title="Fetch Data"
+        onPress={() => {
+          fetchData()
+        }}
       />
     </View>
-  );
-};
+  )
+}
 
-export default MovieDetail;
+export default MovieDetail
